7e5c6f9e5af0ec3ef4746ae8c7dfb38995781657
Shubham <shubham4315@gmail.com>
2021-06-08 22:47:41 +0200

    Creating a React Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(){
+  return(
+    <h1>Eve's Kitchen</h1>
+  );
+}
+
+function Main(){
+  return(
+    <p>we serve the most delicious food around.</p>
+  );
+}
+
+function Footer(){
+  return(
+    <p>It's True.</p>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,26 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  React.createElement("h1",{style:{color:"blue"}},"Hello,World!"),
-  document.getElementById('root')
-);
-
-ReactDOM.render(
-  React.createElement("ul",null,
-  React.createElement("li",null,"Monday"),
-  React.createElement("li",null,"Tuesday"),
-  React.createElement("li",null,"Wednesday"),
-  React.createElement("li",null,"Thursday"),
-  React.createElement("li",null,"Friday"),
-  React.createElement("li",null,"Saturday"),
-  ),
-  document.getElementById('root')
-);
-
-ReactDOM.render(
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-  </ul>,
+  <App/>,
   document.getElementById('root')
 );
