0ff632d3316e92bf17d9600639944b8169ddc4d7
Shubham <shubham4315@gmail.com>
2021-06-08 18:20:11 +0200

    First steps to React.createElement()

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement("h1",{style:{color:"blue"}},"Hello,World!"),
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  React.createElement("ul",null,
+  React.createElement("li",null,"Monday"),
+  React.createElement("li",null,"Tuesday"),
+  React.createElement("li",null,"Wednesday"),
+  React.createElement("li",null,"Thursday"),
+  React.createElement("li",null,"Friday"),
+  React.createElement("li",null,"Saturday"),
+  ),
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+  </ul>,
+  document.getElementById('root')
+);
