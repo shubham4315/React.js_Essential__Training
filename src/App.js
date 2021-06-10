e288475059e553ad47129ef0c4a99aa308bf176a
Shubham <shubham4315@gmail.com>
2021-06-10 23:09:32 +0200

    Handling API calls

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import './App.css';
 import restaurant from './restaurant.jpeg'
-import React,{useReducer} from "react";
-function App() {
-  const [checked, toggle] = useReducer(
-    (checked)=>!checked,
-    false);
+import React,{useState,useEffect} from "react";
 
+function App({login}) {
+  const [data,setData] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const [error,setData] = useState(null);
+
+useEffect(()=>{
+  if(!login)
+  {
+    return;
+  }
+  setLoading(true);
+  fetch(`https://api.github.com/users/${login}`).then(
+    response =>response.json()).then(
+      setData
+    ).then(
+      ()=>setLoading(false)
+    ).catch(
+      setError()
+    );
+  },[login]
+);
+
+if(loading){
   return(
-    <>
-    <input type="checkbox" value={checked} onChange={toggle}/>
-    <p>{checked?"checked":"not checked"}</p>
-    </>
+    <h1>Loading....</h1>
   );
+}
+
+if(error){
+  return(
+    <pre>{JSON.stringify(error,null,2)}</pre>
+  )
+}
+
+if(!data){
+  return null;
+}
+
+return(
+  <div>
+    <h1>{data.name}</h1>
+    <p>{data.location}</p>
+    <img alt="avatar" src={data.avatar_url}/>
+  </div>
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <App/>,
+    <App login="shubham4315"/>,
   document.getElementById('root')
 );
